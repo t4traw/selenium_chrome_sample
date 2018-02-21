cfef2282a5b49b315deabcdf7d04f1614d50737c
Tatsuro Moriyama <t4traw@gmail.com>
2018-02-21 12:36:43 +0900

    試しにGoogleにアクセスしてみる

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'bundler'
+Bundler.require
+
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--headless')
+driver = Selenium::WebDriver.for :chrome, options: options
+
+driver.navigate.to 'https://www.google.co.jp/'
+driver.save_screenshot './google.jpg'
