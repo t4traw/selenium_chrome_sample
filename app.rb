e84e50303687ac549316a4d8083aca470357a2a3
Tatsuro Moriyama <t4traw@gmail.com>
2018-02-21 14:28:51 +0900

    IDを入力してボタンをクリックさせる

--- app.rb
+++ app.rb
@@ -13,3 +13,8 @@ yahoo_application_id = ENV['YAHOO_APPLICATION_ID']
 YAHOO_AUTH_URL = "https://auth.login.yahoo.co.jp/yconnect/v1/authorization?response_type=code+id_token&client_id=#{yahoo_application_id}&state=foobar&redirect_uri=oob&nonce=hogehoge"
 driver.navigate.to YAHOO_AUTH_URL
 driver.save_screenshot './yahoo-1.png'
+
+element = driver.find_element(:name, 'login')
+element.send_keys(ENV['YAHOO_USER_ID'])
+driver.find_element(:name, 'btnNext').click
+driver.save_screenshot './yahoo-2.png'
