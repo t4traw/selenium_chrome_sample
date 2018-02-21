6cead9fe6d14821b4d8c8ad5683e348cba77e3bb
Tatsuro Moriyama <t4traw@gmail.com>
2018-02-21 12:58:47 +0900

    Yahooの認証ページに繋ぐ

--- app.rb
+++ app.rb
@@ -8,3 +8,8 @@ driver = Selenium::WebDriver.for :chrome, options: options
 
 driver.navigate.to 'https://www.google.co.jp/'
 driver.save_screenshot './google.png'
+
+yahoo_application_id = ENV['YAHOO_APPLICATION_ID']
+YAHOO_AUTH_URL = "https://auth.login.yahoo.co.jp/yconnect/v1/authorization?response_type=code+id_token&client_id=#{yahoo_application_id}&state=foobar&redirect_uri=oob&nonce=hogehoge"
+driver.navigate.to YAHOO_AUTH_URL
+driver.save_screenshot './yahoo-1.png'
